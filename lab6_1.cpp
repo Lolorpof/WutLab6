85e69bcd6e0c0e1fb0de4806dab9a32771b4aa4d
Lolorpof <sirawit_sir@cmu.ac.th>
2022-12-09 15:11:50 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,22 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int i, even_count = 0, odd_count = 0;
+    do{
+        cout << "Enter an integer: ";
+        cin >> i;
+        if(i == 0){
+            break;
+        }
+        else if(i%2 == 0){
+            even_count++;
+        }
+        else{
+            odd_count++;
+        }
+    }while(i != 0);
+
+    cout << "#Even numbers = " << even_count << "\n";
+    cout << "#Odd numbers = " << odd_count;
     return 0;
 }
